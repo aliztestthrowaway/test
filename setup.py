f7e221703d472621f29d0932aff95847c3b77de2
Denes Panta <npdeey@gmail.com>
2019-09-01 23:56:28 +0200

    first commit

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
       name = 'aliz_test',
       version = '1.0',
       description = 'Custom Log. Reg. Estimator',
-      url = 'http://github.com/storborg/funniest',
+      url = 'https://github.com/aliztestthrowaway/test',
       author = 'Denes Panta',
       author_email = 'example@example.com',
       license = 'MIT',
-      packages=['aliz'],
+      packages = ['aliz'],
       zip_safe = False
       )
